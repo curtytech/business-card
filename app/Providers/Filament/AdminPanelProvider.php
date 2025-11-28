710fb53f7bb7152f78bba40682015ae2e9afb59d
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2025-11-28 19:21:49 -0300

    qr code

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,6 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 \App\Filament\Widgets\UsuariosCardWidget::class,
+                \App\Filament\Widgets\UserQrWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
